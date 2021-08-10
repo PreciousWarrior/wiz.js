2d1079ba9e2b5ca1f6ec76e89bb45d94ff2849c3
PreciousWarrior / IamPrecious <61111041+PreciousWarrior@users.noreply.github.com>
2021-08-10 23:40:40 +0530

    add typedefs

---
+++ src/typedefs.js
@@ -0,0 +1,20 @@
+/**
+ * @typedef {Object} User
+ * @property {string} id - The user ID
+ * @property {string} email - The user email
+ * @property {string} type - The user type
+ * @property {string} first_name: - The user's first name
+ * @property {string} middle_name: - The user's middle name
+ * @property {string} last_name: - The user's last name
+ * @property {string} school: - The ID of the user's school
+ * @property {string} gender: - The user's gender
+ * @property {string} image: - An HTTP path to the user's image
+ * @property {string} school_name: - The name of the user's school
+ *
+ * @typedef {Object} auth
+ * @property {string} email - Email ID of the user
+ * @property {string} password - Password of the user
+ * @property {Number} school - School ID (0-2) of the user
+ */
+
+export default {};
