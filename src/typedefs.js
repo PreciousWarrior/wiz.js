8cec2ac698fe5d717d79e75f3531ae14801623fa
PreciousWarrior / IamPrecious <61111041+PreciousWarrior@users.noreply.github.com>
2021-08-15 01:57:12 +0530

    fix typedefs issue

--- src/typedefs.js
+++ src/typedefs.js
@@ -10,12 +10,15 @@
  * @property {string} gender: - The user's gender
  * @property {string} image: - An HTTP path to the user's image
  * @property {string} school_name: - The name of the user's school
- *
- * @typedef {Object} auth
+ */
+/**
+ *  @typedef {Object} auth
  * @property {string} email - Email ID of the user
  * @property {string} password - Password of the user
  * @property {Number} school - School ID (0-2) of the user
- *
+ */
+
+/**
  * @typedef {Object} Meeting
  * @property {string} id - The ID of the meeting
  * @property {string} topic - The name of the class the zoom meeting is for
@@ -25,7 +28,9 @@
  * @property {string} password - The password to join the meeting
  * @property {string} host - The host of the meeting
  * @property {string} agenda - The agenda of the meeting
- *
+ */
+
+/**
  * @typedef {Object} Schedule
  * @property {Number} id - The ID of the class
  * @property {Number} startTime - The start time of the class, represented by the number of milliseconds that have elapsed since 1970-01-01 00:00:00 UTC
@@ -34,14 +39,18 @@
  * @property {string} teacherName - The name of the teacher of the class
  * @property {string} subject - The subject of the class
  * @property {string} code - The code of the class
- *
+ */
+
+/**
  * @typedef {Object} Class
  * @property {Number} id - The ID of the class
  * @property {string} name - The name of the class
  * @property {string} code - The code of the class
  * @property {Boolean} isAcademic - Whether the class is academic or not
  * @property {Number} grade - The grade of the class
- *
+ */
+
+/**
  * @typedef {Object} ClassFromList
  * @property {string} program - The program (MYP, DP, etc) of the class
  * @property {Number} grade - The grade of the class
