af98e37bf844b3dcdbec55c6ee26881e2a17e0df
PreciousWarrior / IamPrecious <61111041+PreciousWarrior@users.noreply.github.com>
2021-08-15 15:57:24 +0530

    remove documentation dates, make docs auto generate, add types import in index.js to fix intellisense

--- gendocs.sh
+++ gendocs.sh
@@ -2,19 +2,16 @@
 paths="typedefs.js clients/BaseClient.js clients/StudentClient.js"
 
 rm -r build
-rm -r docs
 
 mkdir build
-cp -r src build
+cp -r src/* build
 for path in $paths
 do
-    fullpath="build/src/$path"
+    fullpath="build/$path"
     echo "Removing occourences of 'Types.' in $fullpath"
     sed -i "s/Types\.//" $fullpath
 done
 
-cd build/src
-jsdoc -d '../docs' $paths
+cd build
+../node_modules/.bin/jsdoc -d ../docs -c ../jsdoc.config.json $paths
 cd ..
-cd ..
-mv build/docs .
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Client from "./clients/BaseClient";
 import Student from "./clients/StudentClient";
+import * as Types from "./typedefs.js";
 
 export { Client, Student };
