4dcdc9678796d2884fdea1a43912e6585c2e344e
PreciousWarrior / IamPrecious <61111041+PreciousWarrior@users.noreply.github.com>
2021-09-02 13:37:11 +0530

    remove unnecessary console.log

--- src/clients/StudentClient.test.js
+++ src/clients/StudentClient.test.js
@@ -1,22 +1,19 @@
-import auth from '../data/auth'
-import Student from '../clients/StudentClient'
+import auth from "../data/auth";
+import Student from "../clients/StudentClient";
 
-let student
+let student;
 
-jest.setTimeout(150000)
+jest.setTimeout(150000);
 beforeAll(async () => {
-    const currTime = Date.now()
-    console.log('started building student')
-    student = await Student.build(auth)
-    console.log(`Student build in: ${Date.now() - currTime}ms`)
-})
+  student = await Student.build(auth);
+});
 
-test('Tests if meetings object is returned correctly', async () => {
-    //TODO
-    const meetings = await student.getMeetings()
-})
+test("Tests if meetings object is returned correctly", async () => {
+  //TODO
+  const meetings = await student.getMeetings();
+});
 
-test('Should return class information', async () => {
-    //TODO
-    const classInfo = await student.getClass(11614)
-})
+test("Should return class information", async () => {
+  //TODO
+  const classInfo = await student.getClass(11614);
+});
